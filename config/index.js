0721987a3743c20d5bcc1d22db84abc0092de09a
gimmy <975402925@qq.com>
2020-04-03 00:54:22 +0800

    修改生产配置

--- config/index.js
+++ config/index.js
@@ -21,7 +21,7 @@ module.exports = {
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
-    websocketUrl: '"ws://54.144.207.148:8888"',
+    websocketUrl: '"//54.144.207.148:8888"',
     productionSourceMap: false,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
